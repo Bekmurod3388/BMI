24d03de42b73d88d62cce2c8a17e01b46709551a
Ozodbek <ozodbekozodov0002@gmail.com>
2023-01-26 22:57:40 +0500

    admin panel finished

--- routes/web.php
+++ routes/web.php
@@ -24,8 +24,6 @@ Route::get('/admin', function () {
 
 
 
-
-
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
