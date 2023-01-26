f7884aeef0efe16cf95bf22c58842d142d51fd92
Ozodbek <ozodbekozodov0002@gmail.com>
2023-01-26 22:49:03 +0500

    admin panel finished

--- routes/web.php
+++ routes/web.php
@@ -16,16 +16,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('/');
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-
-
-
-
+Route::get('/admin', function () {
+    return view('admin.master');
+})->middleware(['auth', 'verified'])->name('admin');
 
 
 
